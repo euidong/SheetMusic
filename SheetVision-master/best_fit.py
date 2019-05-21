2f88d9f5db8d935c8d27e76abb1004df222ffdba
AnYoungMin <49145596+AnYoungMin@users.noreply.github.com>
2019-05-21 23:17:57 +0900

    음자리표
    높은음자리표, 낮은음자리표추출 clef.py로 추가

--- SheetVision-master/best_fit.py
+++ SheetVision-master/best_fit.py
@@ -8,26 +8,33 @@ def fit(img, templates, start_percent, stop_percent, threshold): # (전체이미
     best_locations = []
     best_scale = 1
 
+    '''
     plt.axis([0, 2, 0, 1]) # 세로축의 값을 의미합니다.
     plt.show(block=False)
 
     x = []
     y = []
+    '''
+
     for scale in [i/100.0 for i in range(start_percent, stop_percent + 1, 3)]: # 3pixel만큼 뛰면서 검색 (50,150)
         locations = []
         location_count = 0
         for template in templates:
             template = cv2.resize(template, None,
                 fx = scale, fy = scale, interpolation = cv2.INTER_CUBIC) # 이미지를 축소 합니다.
-            result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED) # 원본 이미지에서 특정 이미지를 찾는 방법
-            result = np.where(result >= threshold) #유사도가 threshold 이상이면 해당 위치값을 저장합니다.
-            location_count += len(result[0])
-            locations += [result]
+            template_width,template_height=template.shape[::-1]
+            if img_height>=template_height and img_width>=template_width:
+                result = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+                result = np.where(result >= threshold)
+                location_count += len(result[0])
+                locations += [result]
+        '''
         print("scale: {0}, hits: {1}".format(scale, location_count))
         x.append(location_count)
         y.append(scale)
         plt.plot(y, x)
         plt.pause(0.00001)
+        '''
         if (location_count > best_location_count):
             best_location_count = location_count
             best_locations = locations
@@ -35,6 +42,7 @@ def fit(img, templates, start_percent, stop_percent, threshold): # (전체이미
             plt.axis([0, 2, 0, best_location_count])
         elif (location_count < best_location_count):
             pass
+            
     plt.close()
 
     return best_locations, best_scale
